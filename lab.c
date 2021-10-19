87b0834a290441f1a51450c9c7cd84780820a9a3
DiPirs <90197693+DiPirs@users.noreply.github.com>
2021-10-19 11:00:59 +0300

    rand_numbers
    https://replit.com/join/clknrumvge-darkus12 - для тестирования

---
+++ lab.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    int             n, i;                                           // переменные для счета генерации и для проверки индекса
+    double          max, min, part_chast, a;                        // две переменных диапозона, переменная с дробной частью и переменная для генерации
+    double          number, sum = 0;                                // переменная для целой части, переменная для подсчёта суммы
+
+    printf("Input n: ");
+    scanf_s("%d", &n);
+    printf("Input min: ");
+    scanf_s("%lf", &min);
+    printf("Input max: ");
+    scanf_s("%lf", &max);
+
+    for (i = 0; i < n; i++)
+    {
+        a = (double)rand() / RAND_MAX * (max - min) + min;          // генерирует число
+        part_chast = modf(a, &number);                              // находит дробную часть
+
+        if (i >= 0 && i < 10 && i != (int)(part_chast * 10))
+            sum += a;
+        else if (i >= 10 && i < 100 && i != (int)(part_chast * 100))
+            sum += a;
+        else if (i >= 100 && i < 1000 && i != (int)(part_chast * 1000))
+            sum += a;
+        else if (i >= 1000 && i < 10000 && i != (int)(part_chast * 10000))
+            sum += a;
+        else if (i >= 10000 && i < 100000 && i != (int)(part_chast * 100000))
+            sum += a;
+    }
+    printf("SUM: %f\n\n", sum);
+
+    return 0;
+}
