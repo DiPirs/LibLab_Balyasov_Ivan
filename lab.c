624d41c4aee6eae14f1667da0f8bee74716e3072
DiPirs <90197693+DiPirs@users.noreply.github.com>
2021-11-01 12:11:52 +0300

    Update lab.c
    https://replit.com/join/clknrumvge-darkus12 - для теста

--- lab.c
+++ lab.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <malloc.h>
 
- 
+#define _CRT_SECURE_NO_WARNINGS
+
 int main(void)
 {
-    int             n, i;                                           // переменные для счета генерации и для проверки индекса
-    double          max, min, part_chast, a;                        // две переменных диапозона, переменная с дробной частью и переменная для генерации
-    double          temp, number, sum = 0;                          // переменная для перестановки, переменная для целой части, переменная для подсчёта суммы
+    int             n, i;                                           
+    double          max, min, part_chast, a;                        
+    double          number, sum = 0;                          
+    double*         mas;
+
+    system("chcp 1251");
+    system("cls");
 
     printf("Input n: ");
     scanf_s("%d", &n);
@@ -15,30 +21,38 @@ int main(void)
     scanf_s("%lf", &min);
     printf("Input max: ");
     scanf_s("%lf", &max);
-    
-    if (min > max){                                                 // Если случайно были перепутаны min и max при вводе, производится перестановка данных
-        temp = min;
-        min = max;
-        max = temp;
+
+    mas = (double*)malloc(n * sizeof(double));
+
+    for (i = 0; i < n; i++)
+    {
+        a = (((double)rand()) / RAND_MAX) * (max - min) + min;
+        mas[i] = a;
     }
 
     for (i = 0; i < n; i++)
     {
-        a = (double)rand() / RAND_MAX * (max - min) + min;          // генерирует число
-        part_chast = modf(a, &number);                              // находит дробную часть
+        part_chast = modf(mas[i], &number);
 
-        if (i >= 0 && i < 10 && i != (int)(part_chast * 10))        // Проверка на совпадение дробной части с номером этого числа
-            sum += a;
-        else if (i >= 10 && i < 100 && i != (int)(part_chast * 100))
-            sum += a;
-        else if (i >= 100 && i < 1000 && i != (int)(part_chast * 1000))
-            sum += a;
-        else if (i >= 1000 && i < 10000 && i != (int)(part_chast * 10000))
-            sum += a;
-        else if (i >= 10000 && i < 100000 && i != (int)(part_chast * 100000))
-            sum += a;
+        if (i >= 0 && i < 10 && i == (int)(part_chast * 10))
+            sum -= mas[i];
+        else if (i >= 10 && i < 100 && i == (int)(part_chast * 100))
+            sum -= mas[i];
+        else if (i >= 100 && i < 1000 && i == (int)(part_chast * 1000))
+            sum -= mas[i];
+        else if (i >= 1000 && i < 10000 && i == (int)(part_chast * 10000))
+            sum -= mas[i];
+        else if (i >= 10000 && i < 100000 && i == (int)(part_chast * 100000))
+            sum -= mas[i];
+        else  
+            sum += mas[i];
+            
     }
+
     printf("SUM: %f\n\n", sum);
- 
+
+    free(mas);
+
     return 0;
 }
+
